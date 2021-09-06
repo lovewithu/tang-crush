e749baf44ec98e50f5c107c49d3ab55129ed56ec
lovewithu <90173740+lovewithu@users.noreply.github.com>
2021-09-06 12:53:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'Cục cưng à!',
+    introDesc: `Nếu em không ngại 
+    thì để ngày mai anh qua thưa ba má em cho mình làm quenn,
+    biết đâu sớm mai thức dậy nhìn thấy nhau
+    Cất lên tiếng gọi “Mình ơi!”`,
+    btnIntro: 'Nghĩ thôi cũng vui,hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Vẫn cứ là yêu anh, cưới anh nhó <33',
+    btnNo: 'Không, yêu à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'Ráng đợi hết dịch gòi tụi mình vào làm đứa nhó',
+    btnAccept: 'Okiiiii lun <33',
+    messLink: 'https:fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
